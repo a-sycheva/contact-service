4e1a87f34d47b460b1bf5be27f867dae7dc62589
Anastasiya Sycheva <a.a.sycheva95@yandex.ru>
2026-05-05 17:11:25 +0300

    initial commit: bootstrap

---
+++ src/main/java/ru/mentee/power/crm/contactservice/ContactServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.mentee.power.crm.contactservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ContactServiceApplication.class, args);
+  }
+}
