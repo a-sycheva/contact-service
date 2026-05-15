2ac8b7fee48fad10e6b6ab1632fdd9d2d86d9ff5
Anastasiya Sycheva <a.a.sycheva95@yandex.ru>
2026-05-15 15:41:57 +0300

    LAZY feychType для ManyToOne чтобы избежать n+1 проблемы

--- src/main/java/ru/mentee/power/crm/contactservice/adapter/out/persistence/entity/PersonCompanyLinkEntity.java
+++ src/main/java/ru/mentee/power/crm/contactservice/adapter/out/persistence/entity/PersonCompanyLinkEntity.java
@@ -2,6 +2,7 @@ package ru.mentee.power.crm.contactservice.adapter.out.persistence.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,11 +28,11 @@ public class PersonCompanyLinkEntity {
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "person_id", nullable = false)
   private PersonEntity person;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "company_id", nullable = false)
   private CompanyEntity company;
 
